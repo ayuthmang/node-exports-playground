0d68db86245b3dd1777c4a08a8fda868d1607e5e
Ayuth Mangmesap <ayuth.mang@gmail.com>
2018-08-08 23:51:37 +0700

    Add playground files

---
+++ index.js
@@ -0,0 +1,4 @@
+const math = require('./math');
+
+console.log(math.add(10, 20)); // expected: 30
+console.log(math.multiply(10, 20));  // expected: 200
---
+++ math.js
@@ -0,0 +1,12 @@
+// You can export using
+const add = (x, y) => x + y;
+const multiply = (x, y) => x * y;
+
+module.exports = {
+  add,
+  multiply
+};
+
+// or in this way
+// module.exports.add = (x, y) => x + y;
+// module.exports.multiply = (x, y) => x * y;
